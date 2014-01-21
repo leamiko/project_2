a299c6cd362425a5604d9aa55ddd93e0733023c5
lzjjie <lzjjie@163.com>
2014-01-21 19:41:00 +0800

    update main entrance file
    add NO_CACHE_RUNTIMR const variable

--- index.php
+++ index.php
@@ -8,7 +8,8 @@
  * @version 1.0.0
  * @since 1.0.0
  */
-defined('THINK_PATH') || define('THINK_PATH', './ThinkPHP/');
-defined('APP_NAME') || define('APP_NAME', 'app');
-defined('APP_PATH') || define('APP_PATH', './app/');
+define('THINK_PATH', './ThinkPHP/');
+define('APP_NAME', 'app');
+define('APP_PATH', './app/');
+define('NO_CACHE_RUNTIME', true);
 require THINK_PATH . 'ThinkPHP.php';
