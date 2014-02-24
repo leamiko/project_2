d0a6f5abdb3eecc22400876e792c343e1d86daf8
lzjjie <lzjjie@gmail.com>
2014-02-24 18:05:43 +0800

    database backup

--- data/easy_buy.sql
+++ data/easy_buy.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 3.3.10
+-- version 3.3.8
 -- http://www.phpmyadmin.net
 --
 -- 主机: localhost
--- 生成日期: 2014 年 02 月 22 日 09:40
--- 服务器版本: 5.5.35
--- PHP 版本: 5.4.17
+-- 生成日期: 2014 年 02 月 24 日 18:02
+-- 服务器版本: 5.5.34
+-- PHP 版本: 5.3.18
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
@@ -18,6 +18,31 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 --
 -- 数据库: `easy_buy`
 --
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `easy_address`
+--
+
+CREATE TABLE IF NOT EXISTS `easy_address` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `user_id` int(11) NOT NULL COMMENT 'user_id',
+  `name` varchar(30) NOT NULL COMMENT 'consignee name',
+  `phone` varchar(30) NOT NULL COMMENT 'consignee phone',
+  `zip` varchar(30) NOT NULL COMMENT 'zip code',
+  `address` varchar(255) NOT NULL COMMENT 'consignee address',
+  `add_time` int(11) NOT NULL COMMENT 'add time',
+  `update_time` int(11) DEFAULT NULL COMMENT 'update time',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='user address table' AUTO_INCREMENT=2 ;
+
+--
+-- 转存表中的数据 `easy_address`
+--
+
+INSERT INTO `easy_address` (`id`, `user_id`, `name`, `phone`, `zip`, `address`, `add_time`, `update_time`) VALUES
+(1, 1, 'lzjjie', '13437563074', '518029', 'Room #207,2F,No.43,Jinhu 1st Street,Yinhu Road,Luohu District, Shenzhen, P.R.C', 1393227625, 1393228797);
 
 -- --------------------------------------------------------
 
@@ -44,7 +69,7 @@ CREATE TABLE IF NOT EXISTS `easy_admin_user` (
 --
 
 INSERT INTO `easy_admin_user` (`id`, `username`, `password`, `real_name`, `email`, `add_time`, `last_time`, `status`, `desc`, `type`) VALUES
-(1, 'admin', 'e10adc3949ba59abbe56e057f20f883e', 'admin', 'admin@admin.com', 0, 1393060068, 1, 'Administrator!Do not delete!', 1),
+(1, 'admin', 'e10adc3949ba59abbe56e057f20f883e', 'admin', 'admin@admin.com', 0, 1393218935, 1, 'Administrator!Do not delete!', 1),
 (4, 'demo', 'e10adc3949ba59abbe56e057f20f883e', 'demo', '', 1393059963, 1393060050, 1, '', 0);
 
 -- --------------------------------------------------------
@@ -60,9 +85,11 @@ CREATE TABLE IF NOT EXISTS `easy_member` (
   `phone` varchar(30) DEFAULT NULL COMMENT 'user phone',
   `avatar` varchar(255) DEFAULT NULL COMMENT 'user avatar',
   `sex` tinyint(1) DEFAULT NULL COMMENT 'gender（1：man 0：woman）',
+  `is_vip` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'is a vip account(0:no,1:yes)',
   `email` varchar(255) NOT NULL COMMENT 'user email',
   `register_time` int(11) NOT NULL COMMENT 'user register time',
   `last_time` int(11) DEFAULT NULL COMMENT 'user last login time',
+  `upgrade_time` int(11) DEFAULT NULL COMMENT 'upgrade time',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='member table' AUTO_INCREMENT=3 ;
 
@@ -70,6 +97,6 @@ CREATE TABLE IF NOT EXISTS `easy_member` (
 -- 转存表中的数据 `easy_member`
 --
 
-INSERT INTO `easy_member` (`id`, `account`, `password`, `phone`, `avatar`, `sex`, `email`, `register_time`, `last_time`) VALUES
-(1, 'lzjjie', 'e10adc3949ba59abbe56e057f20f883e', NULL, NULL, 1, 'lzjjie@163.com', 1, NULL),
-(2, 'hxk', 'e10adc3949ba59abbe56e057f20f883e', '134565655', NULL, 0, 'dfdfdsfsdf', 0, NULL);
+INSERT INTO `easy_member` (`id`, `account`, `password`, `phone`, `avatar`, `sex`, `is_vip`, `email`, `register_time`, `last_time`, `upgrade_time`) VALUES
+(1, 'lzjjie', 'e10adc3949ba59abbe56e057f20f883e', NULL, NULL, 1, 1, 'lzjjie@163.com', 1, 1393236022, 1393234186),
+(2, 'hxk', 'e10adc3949ba59abbe56e057f20f883e', '134565655', NULL, 0, 0, 'dfdfdsfsdf', 0, NULL, NULL);
