a972ede553f30c354f4ff9a39b2dbe896b0262c8
lzjjie <lzjjie@gmail.com>
2014-04-14 11:01:54 +0800

    nothing

--- index.php
+++ index.php
@@ -11,6 +11,5 @@
 define('THINK_PATH', './ThinkPHP/');
 define('APP_NAME', 'app');
 define('APP_PATH', './app/');
-// define('NO_CACHE_RUNTIME', true);
 define('APP_DEBUG', true);
 require THINK_PATH . 'ThinkPHP.php';
