f3d2805a571884ff2d8f0a55dc40bc594339f104
lzjjie <lzjjie@163.com>
2014-06-04 00:02:46 +0800

    Change push method

--- app/Common/common.php
+++ app/Common/common.php
@@ -8,36 +8,79 @@
  * @since 1.0.0
  */
 
+// function push($message, $receiver_type = 4, $receiver_value = '', $is_system = 1, $goods_id = null) {
+// vendor('jpush.JPushClient');
+// $jPush_config = C('JPush');
+// $client = new JPushClient($jPush_config['app_key'], $jPush_config['master_secret']);
+// $params = array(
+// "receiver_type" => $receiver_type,
+// "receiver_value" => $receiver_value,
+// "sendno" => 1,
+// "send_description" => "",
+// "override_msg_id" => ""
+// );
+// $extras = array(
+// 'is_system' => $is_system
+// );
+// $goods_id && $extras['goods_id'] = $goods_id;
+// $result = $client->sendNotification($message, $params, $extras);
+// return $result->getCode() ? false : true;
+// }
+
 /**
- * Push message to app
+ * Push
  *
  * @param string $message
  *            Message content
- * @param number $receiver_type
- *            Receiver type
+ * @param int $sendno
+ *            Send number
+ * @param int $receiver_type
+ *            Receiver type(1:IMEI(Must special appKeys first),2:tag,3:alias,4:broadcast
  * @param string $receiver_value
- *            Receiver value
- * @param number $is_system
- *            Is system message(1:Yes,0:No)
- * @param string $goods_id
+ *            Relative with Reciever type
+ * @param int $is_system
+ *            (1:yes,0:no)
+ * @param int|null $goods_id
  *            Goods id
  * @return boolean
  */
 function push($message, $receiver_type = 4, $receiver_value = '', $is_system = 1, $goods_id = null) {
     vendor('jpush.JPushClient');
     $jPush_config = C('JPush');
-    $client = new JPushClient($jPush_config['app_key'], $jPush_config['master_secret']);
-    $params = array(
-        "receiver_type" => $receiver_type,
-        "receiver_value" => $receiver_value,
-        "sendno" => 1,
-        "send_description" => "",
-        "override_msg_id" => ""
-    );
-    $extras = array(
-        'is_system' => $is_system
-    );
-    $goods_id && $extras['goods_id'] = $goods_id;
-    $result = $client->sendNotification($message, $params, $extras);
-    return $result->getCode() ? false : true;
+    $url = 'http://api.jpush.cn:8800/v2/push';
+    $param = '';
+    $param .= '&sendno=' . rand(1, 1000);
+    $param .= '&app_key=' . $jPush_config['app_key'];
+    $param .= '&receiver_type=' . $receiver_type;
+    $param .= '&receiver_value=' . $receiver_value;
+    $masterSecret = $jPush_config['master_secret'];
+    $verification_code = md5($sendno . $receiver_type . $receiver_value . $masterSecret);
+    $param .= '&verification_code=' . $verification_code;
+    $param .= '&msg_type=1';
+    $message = json_encode(array(
+        'n_content' => $message,
+        'n_extras' => array(
+            'ios' => array(
+                'content-available' => 1
+            ),
+            'is_system' => $is_system,
+            'goods_id' => $goods_id
+        )
+    ));
+    $param .= '&msg_content=' . $message;
+    $param .= '&platform=' . $jPush_config['platform'];
+    if (empty($url) || empty($param)) {
+        return false;
+    }
+    $postUrl = $url;
+    $curlPost = $param;
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $postUrl);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $curlPost);
+    $data = json_decode(curl_exec($ch));
+    curl_close($ch);
+    return $data->errcode ? false : true;
 }
